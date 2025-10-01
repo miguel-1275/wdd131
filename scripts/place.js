29878660fa35c25e8bd62d751468db15989e676c
miguel-1275 <mariasgalvez@byupathway.edu>
2025-10-01 00:05:34 -0500

    week 03 activity

---
+++ scripts/place.js
@@ -0,0 +1,25 @@
+document.querySelector("#currentyear").textContent = new Date().getFullYear();
+
+const lastModifiedDate = document.lastModified;
+
+document.querySelector("#lastmodified").textContent = `Last modified: ${lastModifiedDate}`;
+
+const temperature = 8;
+const windSpeed = 10;
+const conditions = "Partly Cloudy";
+
+document.getElementById("temperature").textContent = temperature;
+document.getElementById("windspeed").textContent = windSpeed;
+document.getElementById("conditions").textContent = conditions;
+
+function calculateWindChill(tempC, windKmh) {
+    if (tempC <= 10 && windKmh > 4.8) {
+        const wc = 13.12 + 0.6215 * tempC - 11.37 * Math.pow(windKmh, 0.16) + 0.3965 * tempC * Math.pow(windKmh, 0.16);
+        return Math.round(wc);
+    } else {
+        return tempC;
+    }
+}
+
+const windChill = calculateWindChill(temperature, windSpeed);
+document.getElementById("windchill").textContent = windChill;
