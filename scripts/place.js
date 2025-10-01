20b321d9aad0528c31fdbec1f9e1fe8e7de59a62
miguel-1275 <mariasgalvez@byupathway.edu>
2025-10-01 00:11:13 -0500

    windchill formula fixed

--- scripts/place.js
+++ scripts/place.js
@@ -4,7 +4,7 @@ const lastModifiedDate = document.lastModified;
 
 document.querySelector("#lastmodified").textContent = `Last modified: ${lastModifiedDate}`;
 
-const temperature = 8;
+const temperature = 22;
 const windSpeed = 10;
 const conditions = "Partly Cloudy";
 
@@ -15,9 +15,9 @@ document.getElementById("conditions").textContent = conditions;
 function calculateWindChill(tempC, windKmh) {
     if (tempC <= 10 && windKmh > 4.8) {
         const wc = 13.12 + 0.6215 * tempC - 11.37 * Math.pow(windKmh, 0.16) + 0.3965 * tempC * Math.pow(windKmh, 0.16);
-        return Math.round(wc);
+        return `${Math.round(wc)} °C`;
     } else {
-        return tempC;
+        return `N/A`;
     }
 }
 
